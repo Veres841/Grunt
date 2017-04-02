39c55c45c191e2e21b9bfb31b85e86e5485a1f6b
Veres84 <igorveres34@gmail.com>
2017-04-03 02:23:35 +0300

    Add watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,14 +53,28 @@ module.exports = function(grunt) {
                     'dest/style.min.css': ['css/style.css', 'css/style2.css']
                 }
             }
+        },
+        watch: {
+            scripts: {
+                 files: ['js/*.js'],  
+                 tasks: ['jshint', 'concat', 'uglify'] 
+            },
+            css: {
+                 files: ['css/*.css'],  
+                 tasks: ['cssmin']
+            }
+              
         }
     });
+    
+    
     
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin', 'watch']);
     grunt.registerTask('test', ['']);
 };
